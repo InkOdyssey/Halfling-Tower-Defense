57320e6da9a4fc9dc3d7b271b34998bd1efa1f23
costotoad <vkruger@ftstudent.org>
2026-02-19 12:29:33 -0500

    Added tower zones and scenes
    issue with tower deleting them selves, not all towers are done

---
+++ halfling-tower-defence/Scripts/Blackbeard.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class Blackbeard : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("test");
+	}
+	
+	
+		public void in_range(Node2D body)
+	{
+		GD.Print("in zone");
+		//if (Node2D body is Enemy);
+			{
+				QueueFree();
+			}
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
