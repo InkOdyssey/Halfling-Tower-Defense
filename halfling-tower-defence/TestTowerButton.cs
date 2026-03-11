962c3a88c6c4d245e0421d759a1ebd9ed7f9ad02
thatoneguy69696969 <atom@ftstudent.org>
2026-03-11 12:21:27 -0400

    created placement hitbox and checks

---
+++ halfling-tower-defence/TestTowerButton.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class TestTowerButton : Control
+{
+	private Node2D map;
+	private Node2D create_test_tower;
+	private PackedScene test_tower = GD.Load<PackedScene>("res://Scenes/test_tower.tscn");
+	
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		map = GetNode<Node2D>("/root/map");
+		create_test_tower = test_tower.Instantiate<Node2D>();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+	
+	
+	private void _on_button_pressed()
+		{
+			map.AddChild(create_test_tower);
+		}
+}
