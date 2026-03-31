6783ae33e558a44ab5bdf17d126a32c95be8b27c
costotoad <vkruger@ftstudent.org>
2026-03-31 12:23:31 -0400

    Bug fixing and reorganizing

--- halfling-tower-defence/Scripts/Enemies/TestEnemy.cs
+++ halfling-tower-defence/Scripts/Enemies/TestEnemy.cs
@@ -1,39 +1,52 @@
 using Godot;
 using System;
 
-public partial class TestEnemy2 : CharacterBody2D
+public partial class TestEnemy : CharacterBody2D
 {
+	
+	protected int health = 300;
+	protected bool damage = false;
+	protected Area2D hitArea;
 
 	//assigns pathprogress as a variable, but no value
-	private PathFollow2D pathprogress;
-	private int health = 3000;
-	private Area2D hitArea;
-	
+	protected PathFollow2D pathprogress;
+
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		hitArea = GetNode<Area2D>("hit_area");
 		//gives pathprogress a value
 		pathprogress = GetParent<PathFollow2D>();
+		hitArea = GetNode<Area2D>("hit_area");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (health < 1)
+		{
+			QueueFree();
+		}
 		
 		
 		if (pathprogress.ProgressRatio < 1.0f)
 			{
-				pathprogress.ProgressRatio += .0006f;
+				pathprogress.ProgressRatio += .001f;
 			}
 		else if (pathprogress.ProgressRatio == 1.0f)
 			{
 				GD.Print("freed");
 				QueueFree();
-			}}
+			}
+			
+	}
+	
 
 
-	public void Damage() 
+	
+
+	public virtual void ApplyDamage() 
 	{
 		var bodies = hitArea.GetOverlappingAreas();
 		foreach (var body in bodies)
@@ -41,10 +54,9 @@ public partial class TestEnemy2 : CharacterBody2D
 			if (body is DetectorS)
 			{
 				health -= 1;
-				GD.Print("this is king ");
-
 				if (health < 1)
 				{
+					damage = false;
 					QueueFree();
 				}
 			}
@@ -56,23 +68,25 @@ public partial class TestEnemy2 : CharacterBody2D
 				GD.Print("blackbeard damage");
 				if (health < 1)
 				{
+					damage = false;
 					QueueFree();
 				}
 
 			}	
-		}}
 
 
+			
+		}
+	}
 
+	public virtual void hit_area_leave(Area2D area)
+	{
+	}
 	
-	
-	private void OnKill()
+	public virtual void OnKill()
 	{
 		GD.Print("killed");
 		QueueFree();
-		
+
 	}
-
-
-
 }
---
+++ halfling-tower-defence/Scripts/Enemies/TestEnemy2.cs
@@ -0,0 +1,56 @@
+using Godot;
+using System;
+
+public partial class TestEnemy2 : TestEnemy
+{
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		health = 3000;
+		//call the _Ready function in 'TestEnemy'
+		base._Ready();
+	}
+
+	public override void ApplyDamage()
+	{
+		GD.Print(hitArea.GetOverlappingAreas().Count);
+		base.ApplyDamage();
+	}
+
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+
+		if (pathprogress.ProgressRatio < 1.0f)
+		{
+			pathprogress.ProgressRatio += .0006f;
+		}
+		else if (pathprogress.ProgressRatio == 1.0f)
+		{
+			GD.Print("freed");
+			QueueFree();
+		}
+	}
+
+
+
+
+	public override void hit_area_leave(Area2D area)
+	{
+	}
+
+	
+	
+	public override void OnKill()
+	{
+		GD.Print("killed");
+		QueueFree();
+		
+	}
+
+
+
+}
