c668ab7952594e6ecd397a543dc9c80cfb614065
costotoad <vkruger@ftstudent.org>
2026-04-01 11:34:30 -0400

    reworking spawners

--- halfling-tower-defence/Scripts/Map and Other/WaveButton.cs
+++ halfling-tower-defence/Scripts/Map and Other/WaveButton.cs
@@ -4,12 +4,7 @@ using System;
 public partial class WaveButton : Control
 {
 	private Node2D map;
-	private Node2D create_spawner1;
-	private Node2D create_spawner2;
-	
-	
-	private PackedScene spawner1 = GD.Load<PackedScene>("res://Scenes/spawner1.tscn");
-	private PackedScene spawner2 = GD.Load<PackedScene>("res://Scenes/spawner2.tscn");
+
 	
 	private int wave = 1;
 	
@@ -17,8 +12,7 @@ public partial class WaveButton : Control
 	public override void _Ready()
 	{
 		map = GetNode<Node2D>("/root/map/");
-		create_spawner1 = spawner1.Instantiate<Node2D>();
-		create_spawner2 = spawner2.Instantiate<Node2D>();
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,13 +23,13 @@ public partial class WaveButton : Control
 	
 	private void wave_1()
 		{
-			map.AddChild(create_spawner1);
+
 			wave += 1;
 			GD.Print("wave 1 started");
 		}
 	private void wave_2()
 		{
-			map.AddChild(create_spawner2);
+
 			wave += 1;
 			GD.Print("wave 2 started");
 		}
