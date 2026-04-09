418e9f54f12faa9ca4b038a31b3eb31cdf301758
costotoad <vkruger@ftstudent.org>
2026-04-09 12:03:19 -0400

    Revert "Merge pull request #38 from InkOdyssey/revert-37-ReworkEnemysAttacks"
    This reverts commit c114c077592074d522912034aadfc93dae2c027d, reversing
    changes made to 2daf50dcb012fd180ed2a740e22950338540c26a.

---
+++ halfling-tower-defence/Scripts/Enemies/TestEnemy.cs
@@ -0,0 +1,62 @@
+using Godot;
+using System;
+
+public partial class TestEnemy : CharacterBody2D
+{
+	
+	protected int health = 300;
+	protected bool damage = false;
+	protected Area2D hitArea;
+
+	//assigns pathprogress as a variable, but no value
+	public PathFollow2D pathprogress;
+
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//gives pathprogress a value
+		if (pathprogress is null)
+			pathprogress = GetParent<PathFollow2D>();
+		hitArea = GetNode<Area2D>("hit_area");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+		
+		if (pathprogress.ProgressRatio < 1.0f)
+			{
+				pathprogress.ProgressRatio += .001f;
+			}
+		else if (pathprogress.ProgressRatio == 1.0f)
+			{
+				GD.Print("freed");
+				QueueFree();
+			}
+			
+	}
+
+
+
+
+
+	public void ApplyDamage(int damage)
+	{
+		health -= damage;
+		GD.Print(health);
+		if (health <= 0)
+			OnKill();
+	}
+
+	public virtual void hit_area_leave(Area2D area)
+	{
+	}
+	
+	public virtual void OnKill()
+	{
+		QueueFree();
+	}
+}
--- halfling-tower-defence/Scripts/Map and Other/WaveButton.cs
+++ halfling-tower-defence/Scripts/Map and Other/WaveButton.cs
@@ -4,12 +4,7 @@ using System;
 public partial class WaveButton : Control
 {
 	private Node2D map;
-	private Node2D create_spawner1;
-	private Node2D create_spawner2;
-	
-	
-	private PackedScene spawner1 = GD.Load<PackedScene>("res://Scenes/spawner1.tscn");
-	private PackedScene spawner2 = GD.Load<PackedScene>("res://Scenes/spawner2.tscn");
+
 	
 	private int wave = 1;
 	
@@ -17,8 +12,7 @@ public partial class WaveButton : Control
 	public override void _Ready()
 	{
 		map = GetNode<Node2D>("/root/map/");
-		create_spawner1 = spawner1.Instantiate<Node2D>();
-		create_spawner2 = spawner2.Instantiate<Node2D>();
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,6 +23,7 @@ public partial class WaveButton : Control
 	
 	private void wave_1()
 		{
+
 			var create_spawner1a = spawner1.Instantiate<Node2D>();
 			
 			map.AddChild(create_spawner1a);
@@ -37,6 +32,7 @@ public partial class WaveButton : Control
 		}
 	private void wave_2()
 		{
+
 			var create_spawner2a = spawner2.Instantiate<Node2D>();
 			
 			map.AddChild(create_spawner2a);
