2507efea677d62548e0747c6593d7a085e1ec23d
costotoad <vkruger@ftstudent.org>
2026-04-21 11:50:11 -0400

    changing enemies

--- halfling-tower-defence/Scripts/Enemies/TestEnemy.cs
+++ halfling-tower-defence/Scripts/Enemies/TestEnemy.cs
@@ -4,111 +4,59 @@ using System;
 public partial class TestEnemy : CharacterBody2D
 {
 	
-	private int health = 300;
-	private bool damage = false;
-	private Area2D hitArea;
-	private bool _isDead = false;
-
-
-	
+	protected int health = 300;
+	protected bool damage = false;
+	protected Area2D hitArea;
 
 	//assigns pathprogress as a variable, but no value
-	private PathFollow2D pathprogress;
+	public PathFollow2D pathprogress;
 
 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
-
-
 		//gives pathprogress a value
-		pathprogress = GetParent<PathFollow2D>();
+		if (pathprogress is null)
+			pathprogress = GetParent<PathFollow2D>();
 		hitArea = GetNode<Area2D>("hit_area");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (health < 1)
-		{
-			OnKill();
-			return;
-}
 		
 		
 		if (pathprogress.ProgressRatio < 1.0f)
 			{
 				pathprogress.ProgressRatio += .001f;
 			}
-		else if (pathprogress.ProgressRatio >= 1.0f)
+		else if (pathprogress.ProgressRatio == 1.0f)
 			{
 				GD.Print("freed");
 				QueueFree();
-				if (GameManager.Instance != null)
-					GameManager.Instance.LoseHearts(10);
-				else
-					GD.PrintErr("GameManager.Instance is NULL!");
 			}
 			
 	}
-	
 
 
-	
 
-	public void ApplyDamage() 
+
+
+	public void ApplyDamage(int damage)
 	{
-		var bodies = hitArea.GetOverlappingAreas();
-		foreach (var body in bodies)
-		{
-			if (body is DetectorS)
-			{
-				health -= 1;
-				GD.Print(health);
-				if (health < 1)
-				{
-					damage = false;
-					OnKill();
-				}
-			}
-
-			else if (body is Detector)
-			{
-				health -= 10000;
-				GD.Print(health);
-				GD.Print("blackbeard damage");
-				if (health < 1)
-				{
-					damage = false;
-					OnKill();
-				}
-
-			}	
-
-
-			
-		}
+		health -= damage;
+		GD.Print(health);
+		if (health <= 0)
+			OnKill();
 	}
 
-	public void hit_area_leave(Area2D area)
+	public virtual void hit_area_leave(Area2D area)
 	{
-		GD.Print("enemy zone inactive");
 	}
 	
-	private void OnKill()
-{
-		if (_isDead) return;
-			_isDead = true;
-
-		GD.Print("Enemy killed");
-
-		if (GameManager.Instance != null)
-			GameManager.Instance.AddCoins(5);
-		else
-			GD.PrintErr("GameManager is NULL on kill!");
-
+	public virtual void OnKill()
+	{
 		QueueFree();
-}
 	}
+}
