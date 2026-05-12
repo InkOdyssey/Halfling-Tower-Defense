e1971dacda6439400ee2066d23464788452575a3
costotoad <vkruger@ftstudent.org>
2026-05-12 11:35:05 -0400

    bullet fix

--- halfling-tower-defence/Scripts/Towers/Bullet.cs
+++ halfling-tower-defence/Scripts/Towers/Bullet.cs
@@ -23,6 +23,7 @@ public virtual void ApplyDamage()
 				//replace the apply damage function with bullet.Instantiate()
 				//then have the bullet chase the enemy and apply damage once they hit each other
 				Enemy.ApplyDamage(5);
+				QueueFree();
 				return;
 			}
 		}
