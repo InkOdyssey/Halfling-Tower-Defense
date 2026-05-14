8f687bec7e1ae76f5403baee8271483558569076
costotoad <vkruger@ftstudent.org>
2026-05-14 11:26:46 -0400

    flag update

--- halfling-tower-defence/Scripts/Enemies/Flag.cs
+++ halfling-tower-defence/Scripts/Enemies/Flag.cs
@@ -10,6 +10,7 @@ public partial class Flag : CharacterBody2D
 	private Area2D hitArea;
 	private bool _isDead = false;
 	protected Sprite2D sprite;
+	private Timer timer;
 	
 	
 	
@@ -22,7 +23,8 @@ public partial class Flag : CharacterBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		timer = GetNode<Timer>("Timer");
+		timer.Timeout += _on_timer_timeout;
 		//gives pathprogress a value
 		pathprogress = GetParent<PathFollow2D>();
 		hitArea = GetNode<Area2D>("hit_area");
@@ -64,6 +66,7 @@ public partial class Flag : CharacterBody2D
 		health -= damage;
 		GD.Print(health);
 		sprite.SelfModulate = new Color(1.5f, .5f, .5f, 1f);
+		timer.Start();
 		if (health <= 0)
 			OnKill();
 	}
@@ -87,4 +90,8 @@ public partial class Flag : CharacterBody2D
 
 		QueueFree();
 }
+	private void _on_timer_timeout()
+	{
+		sprite.SelfModulate = new Color(1f, 1f, 1f, 1f);
+	}
 	}
